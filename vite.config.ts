f8aaa30122166c39ecf65968d8e0c350304ba493
richie-pa <rfpaucar@gmail.com>
2026-05-25 15:58:03 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,35 +3,45 @@ import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-
 function figmaAssetResolver() {
   return {
     name: 'figma-asset-resolver',
-    resolveId(id) {
+
+    resolveId(id: string) {
       if (id.startsWith('figma:asset/')) {
         const filename = id.replace('figma:asset/', '')
-        return path.resolve(__dirname, 'src/assets', filename)
+
+        return path.resolve(
+          __dirname,
+          'src/assets',
+          filename
+        )
       }
     },
   }
 }
 
+const isMobile = process.env.BUILD_TARGET === 'mobile'
+
 export default defineConfig({
-  base: "/splitcloud/",
+  base: isMobile ? './' : '/splitcloud/',
+
   plugins: [
     figmaAssetResolver(),
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
+
+    // The React and Tailwind plugins are both required for Make,
+    // even if Tailwind is not being actively used
     react(),
     tailwindcss(),
   ],
+
   resolve: {
     alias: {
-      // Alias @ to the src directory
       '@': path.resolve(__dirname, './src'),
     },
   },
 
-  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
+  // File types to support raw imports.
+  // Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
